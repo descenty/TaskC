476cd657696198847a69e9dc507c4879ed07034a
deverty <sasha55555k@yandex.ru>
2022-04-22 20:44:54 +0300

    first commit

---
+++ audio.py
@@ -0,0 +1,22 @@
+from disk import *
+
+
+class Audio(Disk):
+    def __init__(self, name: str, genre: str, price: int, performer: str, studio: str, tracks: dict):
+        Disk.__init__(self, name, genre, price)
+        self.name = name
+        self.genre = genre
+        self.price = price
+        self.performer = performer
+        self.studio = studio
+        self.tracks = tracks
+
+    def add_track(self, track_name: str, length: int):
+        self.tracks.app
+
+    def remove_track(self, track_name : str):
+        del self.tracks[track_name]
+
+    def print_tracks(self):
+        for track in self.tracks:
+            print('{} : {}'.format(track[0], track[1]))
---
+++ disk.py
@@ -0,0 +1,5 @@
+class Disk:
+    def __init__(self, name: str, genre: str, price: int):
+        self.name = name
+        self.genre = genre
+        self.price = price
